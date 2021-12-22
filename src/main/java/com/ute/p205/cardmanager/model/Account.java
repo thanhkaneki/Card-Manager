81645c6380b22eaf2edd6c63a2f74f529f9a738c
thanhkaneki <43461011+thanhkaneki@users.noreply.github.com>
2021-12-23 00:19:46 +0700

    Update Account.java

--- src/main/java/com/ute/p205/cardmanager/model/Account.java
+++ src/main/java/com/ute/p205/cardmanager/model/Account.java
@@ -7,28 +7,6 @@ import java.sql.Date;
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Getter @Setter
-public class Account {
-    @Id
-    @Column(name = "ID")
-    @GeneratedValue
-    private int id;
-
-    @Column(name = "Number", unique = true, nullable = false)
-    @NonNull private String number;
-
-    @Column(name = "OpenDate" , nullable = false)
-    @NonNull private String openDate;
-
-    @Column(name = "Balance")
-    @NonNull private String balance;
-
-    @ManyToOne
-    @JoinColumn(name = "type_id", nullable = false)
-    private AccountType accountType;
-    
-    @ManyToOne
-    @JoinColumn(name = "status_id", nullable = false)
-    private AccountStatus accountStatus;
 //
 //    @ManyToOne
 ////    @JoinColumn(name = "sysAccount_id")
