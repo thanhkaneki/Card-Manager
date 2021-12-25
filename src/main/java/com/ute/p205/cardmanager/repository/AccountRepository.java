1463377699fab8ad82ae5815c82a283b050b9188
thanhkaneki <43461011+thanhkaneki@users.noreply.github.com>
2021-12-25 12:46:51 +0700

    update

--- src/main/java/com/ute/p205/cardmanager/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ute.p205.cardmanager.repository;
-
-import com.ute.p205.cardmanager.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByNumber(String number);
-}
